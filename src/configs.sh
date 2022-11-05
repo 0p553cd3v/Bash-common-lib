5991b27c1c26392d6ff00dd2e0cb251210e3b42a
0p553cd3v <0p553cd3v@gmail.com>
2022-11-05 20:28:41 +0100

    [BCL-23] Change run orde again

--- src/configs.sh
+++ src/configs.sh
@@ -14,7 +14,7 @@ function config_count_records_GGA (){
   edebug "Arg 3: $3"
   edebug "Arg 4: $4"
   edebug "Running function: run_func_return 1 $(jq ".$2.$3 | .$4 | length" $1)" 
-  retval= run_func_return 1 "jq $(.$2.$3 | .$4 | length) $1 "
+  retval= run_func_return 1 "jq '.$2.$3 | .$4 | length' $1 "
   edebug "Returning: $retval"
   echo "$retval"
 }
