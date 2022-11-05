c3e4539a3431f4a5feaa394c4540de456a6b2fe1
0p553cd3v <0p553cd3v@gmail.com>
2022-11-05 20:12:57 +0100

    [BCL-23] Adding debug functions

--- src/configs.sh
+++ src/configs.sh
@@ -9,7 +9,13 @@ function config_count_records_GGA (){
   # $3 - Select field name 
   # $4 - Value array name
   retval=""
+  edebug "Arg 1: $1"
+  edebug "Arg 2: $2"
+  edebug "Arg 3: $3"
+  edebug "Arg 4: $4"
+  edebug "Running function: $( run_func_return 1 "$(jq '.$2.$3 | .$4 | length' $1)" )"
   retval=$( run_func_return 1 "$(jq '.$2.$3 | .$4 | length' $1)" )
+  edebug "Returning: $retval"
   echo "$retval"
 }
 
@@ -20,7 +26,14 @@ function config_count_records_GSA (){
   # $4 - Select field value
   # $5 - Value array name
   retval=""
+  edebug "Arg 1: $1"
+  edebug "Arg 2: $2"
+  edebug "Arg 3: $3"
+  edebug "Arg 4: $4"
+  edebug "Arg 5: $5"
+  edebug "Running function: $( run_func_return 1 "$(jq '.$2[] | select(.$3=="$4") | .$5 | length' $1)" )"
   retval=$( run_func_return 1 "$(jq '.$2[] | select(.$3=="$4") | .$5 | length' $1)" )
+  edebug "Returning: $retval"
   echo "$retval"
 }
 
@@ -31,7 +44,14 @@ function config_read_value_GSF (){
   # $4 - Select field value
   # $5 - Value field name
   retval=""
+  edebug "Arg 1: $1"
+  edebug "Arg 2: $2"
+  edebug "Arg 3: $3"
+  edebug "Arg 4: $4"
+  edebug "Arg 5: $5"
+  edebug "Running function: $( run_func 1 "$(jq '.$2[] | select(.$3=="$4") | .$5' $1)" )"
   retval=$( run_func 1 "$(jq '.$2[] | select(.$3=="$4") | .$5' $1)" )
+  edebug "Returning: $retval"
   echo "$retval"
 }
 
@@ -43,7 +63,15 @@ function config_read_value_GSA (){
   # $5 - Value array name
   # $6 - Value array index
   retval=""
+  edebug "Arg 1: $1"
+  edebug "Arg 2: $2"
+  edebug "Arg 3: $3"
+  edebug "Arg 4: $4"
+  edebug "Arg 5: $5"
+  edebug "Arg 6: $6"
+  edebug "Running function: $( run_func 1 "$(jq '.$2[] | select(.$3=="$4") | .$5[$6]' $1)" )"
   retval=$( run_func 1 "$(jq '.$2[] | select(.$3=="$4") | .$5[$6]' $1)" )
+  edebug "Returning: $retval"
   echo "$retval"
 }
 
@@ -54,6 +82,13 @@ function config_read_number_value_GGA (){
   # $4 - Value array name
   # $5 - Value array index
   retval=""
+  edebug "Arg 1: $1"
+  edebug "Arg 2: $2"
+  edebug "Arg 3: $3"
+  edebug "Arg 4: $4"
+  edebug "Arg 5: $5"
+  edebug "Running function: $( run_func 1 "$(jq '.$2.$3 | .$4[$5]' | tonumber $1)" )"
   retval=$( run_func 1 "$(jq '.$2.$3 | .$4[$5]' | tonumber $1)" )
+  edebug "Returning: $retval"
   echo "$retval"
 }
