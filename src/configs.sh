dce8b5f55fcafd36511bc7435f33c2301ee6e44d
0p553cd3v <0p553cd3v@gmail.com>
2022-11-05 21:16:18 +0100

    [BCL-23] Removing debug functions as it messed with return

--- src/configs.sh
+++ src/configs.sh
@@ -9,13 +9,8 @@ function config_count_records_GGA (){
   # $3 - Select field name 
   # $4 - Value array name
   retval=""
-  edebug "Arg 1: $1"
-  edebug "Arg 2: $2"
-  edebug "Arg 3: $3"
-  edebug "Arg 4: $4"
   retval=$(jq -r ".$2.$3.$4 | length" $1)
   check_if_fail
-  edebug "Returning: $retval"
   echo "$retval"
 }
 
@@ -26,14 +21,8 @@ function config_count_records_GSA (){
   # $4 - Select field value
   # $5 - Value array name
   retval=""
-  edebug "Arg 1: $1"
-  edebug "Arg 2: $2"
-  edebug "Arg 3: $3"
-  edebug "Arg 4: $4"
-  edebug "Arg 5: $5"
   retval=$(jq -r ".$2[] | select(.$3==$4) | .$5 | length" $1)
   check_if_fail
-  edebug "Returning: $retval"
   echo "$retval"
 }
 
@@ -44,14 +33,8 @@ function config_read_value_GSF (){
   # $4 - Select field value
   # $5 - Value field name
   retval=""
-  edebug "Arg 1: $1"
-  edebug "Arg 2: $2"
-  edebug "Arg 3: $3"
-  edebug "Arg 4: $4"
-  edebug "Arg 5: $5"
   retval=$(jq -r ".$2[] | select(.$3==$4) | .$5" $1)
   check_if_fail
-  edebug "Returning: $retval"
   echo "$retval"
 }
 
@@ -63,15 +46,8 @@ function config_read_value_GSA (){
   # $5 - Value array name
   # $6 - Value array index
   retval=""
-  edebug "Arg 1: $1"
-  edebug "Arg 2: $2"
-  edebug "Arg 3: $3"
-  edebug "Arg 4: $4"
-  edebug "Arg 5: $5"
-  edebug "Arg 6: $6"
   retval=$(jq -r ".$2[] | select(.$3==$4) | .$5[$6]" $1)
   check_if_fail
-  edebug "Returning: $retval"
   echo "$retval"
 }
 
@@ -82,13 +58,7 @@ function config_read_number_value_GGA (){
   # $4 - Value array name
   # $5 - Value array index
   retval=""
-  edebug "Arg 1: $1"
-  edebug "Arg 2: $2"
-  edebug "Arg 3: $3"
-  edebug "Arg 4: $4"
-  edebug "Arg 5: $5"
   retval=$(jq -r ".$2.$3 | .$4[$5]" | tonumber $1)
   check_if_fail
-  edebug "Returning: $retval"
   echo "$retval"
 }
